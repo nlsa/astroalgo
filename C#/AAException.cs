7b70e39410426c0562ddfedba4328791326a053c
tguillory <tguillory@8cfaf14a-ba43-11dd-a828-c573283ffd12>
2008-11-24 16:28:17 +0000

    added

---
+++ C#/AAException.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TAGDigitalStudios.AstroAlgo
+{
+	/// <summary>
+	/// Summary description for AAException.
+	/// </summary>
+	public class AAException : System.Exception
+	{
+		public AAException(string msg) : base(msg)
+		{
+		}
+	}
+}
